c1ebb43a747eadb73d9320bad49a7a56e4680bdb
Jawaid <mdjawaid00@gmail.com>
2023-01-25 00:19:47 -0800

    connection

---
+++ src/models/subscribers.js
@@ -0,0 +1,19 @@
+const mongoose = require("mangoose");
+
+const subscriberSchema = new mongoose.Schema({
+       name : {
+        type : String,
+        required : true
+       },
+       subscribedChannel : {
+        type : String,
+        required : true,
+       },
+       subscribedDate : {
+        type : Date,
+        required : true,
+        default : Date.now
+       }
+});
+
+module.exports = mongoose.model("Subscriber", subscriberSchema)
